249b0791ce0863dbdeec8b18af8bd4275ebd6c9e
Mark Udit <mudit@bootsoft.com>
2014-08-29 13:59:33 -0400

    final fixes for osx

--- platform/osx/src/FileSystem.cpp
+++ platform/osx/src/FileSystem.cpp
@@ -19,28 +19,29 @@ unsigned long getFileLength(std::ifstream& file)
 }
 
 std::string FileSystem::GetResourceFilePath(std::string resource_file) {
-	return resouce_file;			
+	return resource_file;
 }
 
 unsigned char* FileSystem::LoadFileContents(std::string filename) {
-	file.open(filename, std::ios::in);
-	unsigned long len = getFileLength(file);
-
-	unsigned char* shaderSrc = (unsigned char*) new char[len+1];
-	shaderSrc[len] = 0;  // len isn't always strlen cause some characters are stripped in ascii read...
-	// it is important to 0-terminate the real length later, len is just max possible value...
-
-	unsigned int i=0;
-	while (file.good())
-	{
-		shaderSrc[i++] = file.get();       // get character from file
-		if (i>len) i=len;				   // coding guidelines...
-	}
-
-	shaderSrc[i] = 0;  // 0 terminate it.
-	file.close();
-
-	return shaderSrc;
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    unsigned long len = getFileLength(file);
+    
+    unsigned char* shaderSrc = (unsigned char*) new char[len+1];
+    shaderSrc[len] = 0;  // len isn't always strlen cause some characters are stripped in ascii read...
+    // it is important to 0-terminate the real length later, len is just max possible value...
+    
+    unsigned int i=0;
+    while (file.good())
+    {
+        shaderSrc[i++] = file.get();       // get character from file
+        if (i>len) i=len;                   // coding guidelines...
+    }
+    
+    shaderSrc[i] = 0;  // 0 terminate it.
+    file.close();
+    
+    return shaderSrc;
 }
 
 #endif  // __FileSystem_cpp
